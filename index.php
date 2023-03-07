77a2ae2c1ee576e61b0690bfde3ef66a2ba5ffb8
Gustavo Kang Shim <gkangshim@gmail.com>
2023-03-07 12:57:18 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<marquee> Hi</marquee>
+<link rel="stylesheet" href="main.css">
+<div class="grid-center">
+    <div class="1">
+        aaa
+    </div>
+    <div class="2"> 
+        iiii
+    </div>
+</div>
